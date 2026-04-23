b2ffcaea5394b80eb3cc7bb30dc302439955f55b
Luiz Gustavo da Costa Gonçalves <luizg.dev@gmail.com>
2026-04-23 10:38:33 -0300

    feature(evaluation): cria o golden dataset (dataset de perguntas e respostas para a avaliação)

---
+++ src/evaluation/metrics.py
@@ -0,0 +1,18 @@
+# GOLDEN DATASET
+# Este arquivo serve apenas como base de dados e configuração para a avaliação.
+
+perguntas_teste = [
+    "O que Marco Aurélio afirma ter aprendido com sua mãe?",
+    "Qual é o conselho de Marco Aurélio ao despertar a aurora sobre os encontros do dia a dia?",
+    "De acordo com o Livro II, quais são os três elementos que compõem o ser humano?",
+    "Qual é o refúgio mais tranquilo e com mais calma que o homem pode buscar, segundo o Livro IV?",
+    "O que o texto afirma sobre a glória póstuma e a fama?"
+]
+
+ground_truths = [
+    "Ele aprendeu o respeito aos deuses, a generosidade e a abstenção não somente do agir mal, como também de incorrer em semelhante pensamento; além da frugalidade no regime de vida e o distanciamento do modo de viver próprio dos ricos.",
+    "Ele aconselha a fazer a consideração prévia de que encontrará pessoas indiscretas, ingratas, insolentes, mentirosas, invejosas e não-sociáveis, e que isso lhes ocorre por ignorância sobre o que é o bem e o mal.",
+    "O homem é constituído apenas por um pouco de carne, um breve fôlego vital e o guia interior.",
+    "Em nenhuma parte o homem se retira com maior tranquilidade do que em sua própria alma, sendo este o refúgio que se encontra no pequeno campo de si mesmo.",
+    "A fama póstuma é considerada esquecimento e vaidade total. O homem que se deslumbra com ela não imagina que todos os que o lembrarem também morrerão em breve, sendo a terra inteira apenas um ponto."
+]
